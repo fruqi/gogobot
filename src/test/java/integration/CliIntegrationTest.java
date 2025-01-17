92dbc4f4b3d1966d47139907d798bd39aab99f46
akfaruqi <ext-akf@gc.exchange>
2025-01-17 09:56:29 +0800

    optimize import

--- src/test/java/integration/CliIntegrationTest.java
+++ src/test/java/integration/CliIntegrationTest.java
@@ -7,12 +7,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.util.stream.Collectors;
 
